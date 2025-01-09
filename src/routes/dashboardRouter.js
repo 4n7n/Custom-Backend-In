ee269c7cad8b7985b0b9a1db03a82fbee58de103
4n7n <anthonybonillaparedes7@gmail.com>
2025-01-09 16:35:44 +0100

    npm install date-fns

--- src/routes/dashboardRouter.js
+++ src/routes/dashboardRouter.js
@@ -1,10 +1,41 @@
-import express from "express";
-import { getDashboard } from "../controllers/dashboard/dashboardController.js";
-import { isAuthenticated } from "../middlewares/authMiddleware.js";
+import turnoController from "../appointment/appointmentViewController.js";
+import servicesController from "../services/servicesViewController.js";
 
-const router = express.Router();
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
 
-// Ruta para el dashboard
-router.get("/", isAuthenticated, getDashboard);
+export const getDashboard = async (req, res) => {
+    try {
+        const { turnos, isAdmin } = await turnoController.getAll(req); // Si necesitas isAdmin
+        const { servicios } = await servicesController.getAll(req);
 
-export default router;
+        turnos.forEach((turno) => {
+            let fecha;
+            // Validar si la fecha ya es un objeto Date
+            if (typeof turno.fecha === "string") {
+                // Convertir desde string
+                fecha = new Date(turno.fecha);
+            } else if (turno.fecha instanceof Date) {
+                // Ya es un objeto Date
+                fecha = turno.fecha;
+            } else {
+                throw new Error(`Formato de fecha desconocido: ${turno.fecha}`);
+            }
+
+            var fechaAux = fecha;
+            fechaAux.setHours(fechaAux.getHours() - 1);
+
+            // Formatear la fecha
+            turno.fechaFormateada = format(fechaAux, "EEEE, d 'de' MMMM 'de' yyyy - h:mm a", { locale: es });
+        });
+
+        res.render("dashboard/dashboard", {
+            title: "Panel General",
+            servicios,
+            turnos,
+        });
+    } catch (error) {
+        console.error("Error al obtener los turnos:", error);
+        res.status(500).send("Error al cargar el dashboard.");
+    }
+};
