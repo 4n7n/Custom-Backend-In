49c12ec9242f56d09aced454f6308880b607b001
4n7n <anthonybonillaparedes7@gmail.com>
2025-01-09 15:26:36 +0100

    authViewController.js configurado

---
+++ src/controllers/auth/authViewController.js
@@ -0,0 +1,65 @@
+import authController from "../auth/authController.js";
+
+async function register(req, res) {
+    try {
+        const { username, email, password, passwordConfirm } = req.body;
+        const result = await authController.register(
+            username,
+            email,
+            password,
+            passwordConfirm
+        );
+        res.redirect(
+            "/login?message=usuario registrado correctamente&messageType=success"
+        );
+    } catch (error) {
+        console.error(error);
+        const url = `/register?message=${error.message}&messageType=error`;
+        res.redirect(url);
+    }
+}
+
+
+function loginForm(req, res) {
+    if (req.session.user) {
+        res.redirect("/dashboard");
+    }
+    const { message, messageType } = req.query;
+    console.log("Mensaje: " + message + " Tipo: " + messageType)
+    res.render("user/login", { message, messageType });
+}
+
+function registerForm(req, res) {
+    const { message, messageType } = req.query;
+    res.render("user/register", { message, messageType });
+}
+
+async function login(req, res) {
+    try {
+        const { email, password } = req.body;
+        const user = await authController.login(email, password);
+        req.session.user = {
+            email: user.email,
+            id: user.id,
+            username: user.username,
+            rol: user.rol,
+        };
+        const url = `/dashboard?message=sesión iniciada correctamente&messageType=success`;
+        res.redirect(url);
+    } catch (error) {
+        console.error(error);
+        const url = `/login?message=${error.message}&messageType=error`;
+        res.redirect(url);
+    }
+}
+function logout(req, res) {
+    req.session.user = null;
+    res.redirect("/login");
+}
+export default {
+    register,
+    login,
+    logout,
+    loginForm,
+    registerForm,
+};
