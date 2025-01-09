ed4e28567c220fd89c150cbb092386ac1377d320
4n7n <anthonybonillaparedes7@gmail.com>
2025-01-09 15:31:16 +0100

    userRouter.js configurado

---
+++ src/routes/userRouter.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import userController from "../controllers/user/userViewController.js";
+const router = Router();
+import { isAuthenticated, isAdmin } from "../middlewares/authMiddleware.js";
+
+router.get("/", isAdmin, userController.getAll);
+
+router.get("/:id", userController.getById);
+
+router.post("/new", userController.create);
+
+router.post("/:id/update", userController.update);
+
+router.get("/:id/deactivate", userController.deactivate);
+
+router.get("/:id/activate", userController.activate);
+
+export default router;
