84f3dc9d22e76c33d4972be58ad54c7cdc223ff4
4n7n <anthonybonillaparedes7@gmail.com>
2025-01-23 20:43:04 +0100

    userModel.js actualizado

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,18 +1,68 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/db.js';
+import { Model, DataTypes } from "sequelize";
 
-const Modelo = sequelize.define('modelo', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    nombre: {
-        type: DataTypes.STRING,
-        allowNull: false
+class User extends Model {
+    static init(sequelize) {
+        return super.init(
+            {
+                id: {
+                    type: DataTypes.INTEGER.UNSIGNED,
+                    allowNull: false,
+                    primaryKey: true,
+                    autoIncrement: true,
+                    unique: true,
+                },
+                username: {
+                    type: DataTypes.STRING(40),
+                    allowNull: false,
+                    unique: true,
+                },
+                email: {
+                    type: DataTypes.STRING(255),
+                    allowNull: false,
+                    unique: true,
+                },
+                password: {
+                    type: DataTypes.STRING(50),
+                    allowNull: false,
+                },
+                create_time: {
+                    type: DataTypes.DATE,
+                    allowNull: true,
+                    get() {
+                        const rawValue = this.getDataValue("create_time");
+                        if (!rawValue) return null;
+
+                        // Formatea la fecha en "YYYY-MM-DDTHH:MM"
+                        const fecha = new Date(rawValue);
+                        return fecha.toISOString().slice(0, 16);
+                    },
+                },
+                rol: {
+                    type: DataTypes.ENUM("client", "admin"),
+                    allowNull: true,
+                },
+                active: {
+                    type: DataTypes.TINYINT,
+                    allowNull: false,
+                    defaultValue: 1,
+                },
+            },
+            {
+                sequelize,
+                modelName: "User",
+                tableName: "user", // Nombre exacto de la tabla en la BD
+                timestamps: false, // Si no usas createdAt y updatedAt automÃ¡ticos
+            }
+        );
     }
-}, {
-    tableName: 'nombre_tabla'
-});
 
-export default Modelo;
+    static associate(models) {
+        this.hasMany(models.Turnos, {
+            foreignKey: "user_id",
+            onDelete: "NO ACTION",
+            onUpdate: "NO ACTION",
+        });
+    }
+}
+
+export default User;
